4d28bb25ea9864fe7c07da04bea096b2a579439e
sbx_user1061 <sbx_user1061@ip-10-11-112-179.eu-west-2.compute.internal>
2018-08-05 16:05:02 +0000

    Sun Aug 05 16:05:02 UTC 2018

--- src/BeFaster.App.Tests/Solutions/HLO/HelloSolutionTest.cs
+++ src/BeFaster.App.Tests/Solutions/HLO/HelloSolutionTest.cs
@@ -6,7 +6,7 @@ namespace BeFaster.App.Tests.Solutions.HLO
     [TestFixture]
     public class HelloSolutionTest
     {
-        [TestCase("Craftsman", ExpectedResult = "Hello, World!")]
+        [TestCase("John", ExpectedResult = "Hello, John!")]
         public static string DisplayHelloWorld(string friendName)
         {
             return HelloSolution.Hello(friendName);
