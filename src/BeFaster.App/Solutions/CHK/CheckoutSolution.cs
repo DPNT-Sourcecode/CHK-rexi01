64f93f9f687ca6a3cae4d45a4c5ec01743e7dfb3
sbx_user1061 <sbx_user1061@ip-10-11-112-179.eu-west-2.compute.internal>
2018-08-05 16:42:22 +0000

    Sun Aug 05 16:42:22 UTC 2018

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -1,12 +1,38 @@
-﻿using BeFaster.Runner.Exceptions;
-
-namespace BeFaster.App.Solutions.CHK
+﻿namespace BeFaster.App.Solutions.CHK
 {
     public static class CheckoutSolution
     {
         public static int Checkout(string skus)
         {
-            throw new SolutionNotImplementedException();
+            int totalPrice = 0;
+            string[] items = skus.Split(',');
+            int totalAItems = 0;
+            int totalBItems = 0;
+
+            // calc total price of number of items
+            foreach (string item in items)
+            {
+                switch (item)
+                {
+                    case "A":
+                        totalAItems++;
+                        totalPrice += totalAItems == 3 ? 130 : 50;
+                        break;
+                    case "B":
+                        totalBItems++;
+                        totalPrice += totalBItems == 2 ? 45 : 30;
+                        break;
+                    case "C":
+                        totalPrice += 20;
+                        break;
+                    case "D":
+                        totalPrice += 15;
+                        break;
+                    default:
+                        return -1;
+                }
+            }
+            return totalPrice;
         }
     }
 }
