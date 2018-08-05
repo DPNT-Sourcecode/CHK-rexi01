f52c29103302bb491d47260deae3092611ad91b1
sbx_user1061 <sbx_user1061@ip-10-11-112-179.eu-west-2.compute.internal>
2018-08-05 16:58:32 +0000

    Sun Aug 05 16:58:32 UTC 2018

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -1,25 +1,27 @@
-﻿namespace BeFaster.App.Solutions.CHK
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BeFaster.App.Solutions.CHK
 {
     public static class CheckoutSolution
     {
         public static int Checkout(string skus)
         {
             int totalPrice = 0;
-            string[] items = skus.Split(',');
-            int totalAItems = 0;
-            int totalBItems = 0;
+            IList<string> items = skus.Split(',');
+            int totalAItems = items.Where(x => x.Equals("A")).Count();
+            int totalBItems = items.Where(x => x.Equals("B")).Count();
+            IEnumerable<string> itemsDistinct = items.Distinct();
 
             // calc total price of number of items
-            foreach (string item in items)
+            foreach (string item in itemsDistinct)
             {
                 switch (item)
                 {
                     case "A":
-                        totalAItems++;
                         totalPrice += totalAItems == 3 ? 130 : 50;
                         continue;
                     case "B":
-                        totalBItems++;
                         totalPrice += totalBItems == 2 ? 45 : 30;
                         continue;
                     case "C":
