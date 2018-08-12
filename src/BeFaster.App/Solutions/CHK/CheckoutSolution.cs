e43dee6528faec970f0eb76c81bc589e957d7747
sbx_user1059 <sbx_user1059@ip-10-128-18-126.eu-west-2.compute.internal>
2018-08-12 13:49:49 +0000

    Sun Aug 12 13:49:49 UTC 2018

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -30,26 +30,26 @@ namespace BeFaster.App.Solutions.CHK
                 switch (item)
                 {
                     case 'A':
+                        int totalA = 0;
                         if (totalAItems % 5 == 0 || totalAItems > 5)
                         {
                             // offer: 5A for 200
                             int multiplier = totalAItems / 5;
                             totalPrice += (200 * multiplier);
-                            int totalA = totalAItems - (5 * multiplier);
-                            totalPrice += 50 * totalA;
+                            totalA = totalAItems - (5 * multiplier);
                         }
                         else if (totalAItems % 3 == 0 || totalAItems > 3)
                         {
                             // offer: 3A for 130
                             int multiplier = totalAItems / 3;
                             totalPrice += (130 * multiplier);
-                            int totalA = totalAItems - (3 * multiplier);
-                            totalPrice += 50 * totalA;
+                            totalA = totalAItems - (3 * multiplier);
                         }
-                        if (totalAItems < 3 && totalAItems > 0)
-                            totalPrice += 50 * totalAItems;
+                        if (totalA > 0)
+                            totalPrice += 50 * totalA;
                         continue;
                     case 'B':
+                        int totalB = 0;
                         // offer: buy one get B free
                         if (totalEItems >= 2)
                         {
@@ -61,11 +61,10 @@ namespace BeFaster.App.Solutions.CHK
                             // offer : 2B for 45
                             int multiplier = totalBItems / 2;
                             totalPrice += (45 * multiplier);
-                            int totalB = totalBItems - (2 * multiplier);
-                            totalPrice += 30 * totalB;
+                            totalB = totalBItems - (2 * multiplier);
                         }
-                        if (totalBItems < 2 && totalBItems > 0)
-                            totalPrice += 30 * totalBItems;
+                        if (totalB > 0)
+                            totalPrice += 30 * totalB;
                         continue;
                     case 'C':
                         totalPrice += 20;
