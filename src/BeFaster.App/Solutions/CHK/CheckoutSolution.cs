a2de59fcee08379812dca1ade97c0157a89c9d4f
sbx_user1059 <sbx_user1059@ip-10-128-18-126.eu-west-2.compute.internal>
2018-08-12 20:30:44 +0000

    Sun Aug 12 20:30:44 UTC 2018

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -23,7 +23,7 @@ namespace BeFaster.App.Solutions.CHK
                 {'H', 10},
                 {'I', 35},
                 {'J', 60},
-                {'K', 80},
+                {'K', 70},
                 {'L', 90},
                 {'M', 15},
                 {'N', 40},
@@ -31,14 +31,14 @@ namespace BeFaster.App.Solutions.CHK
                 {'P', 50},
                 {'Q', 30},
                 {'R', 50},
-                {'S', 30},
+                {'S', 20},
                 {'T', 20},
                 {'U', 40},
                 {'V', 50},
                 {'W', 20},
-                {'X', 90},
-                {'Y', 10},
-                {'Z', 50}
+                {'X', 17},
+                {'Y', 20},
+                {'Z', 21}
             };
             Dictionary<string, int> offers = new Dictionary<string, int>()
             {
