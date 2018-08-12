81a639b5c0d62515a2b209195bdf88bbc9622b3d
sbx_user1059 <sbx_user1059@ip-10-128-18-126.eu-west-2.compute.internal>
2018-08-12 19:06:36 +0000

    Sun Aug 12 19:06:36 UTC 2018

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -10,7 +10,7 @@ namespace BeFaster.App.Solutions.CHK
         public static int Checkout(string skus)
         {
             int totalPrice = 0;
-            var skusList = char.Parse(skus);
+            char[] skusChar = skus.ToCharArray();
             Dictionary<char, int> items = new Dictionary<char, int>()
             {
                 {'A', 50},
@@ -40,84 +40,83 @@ namespace BeFaster.App.Solutions.CHK
                 {'Y', 10},
                 {'Z', 50}
             };
-
-            //int totalAItems = items.Where(x => x.Equals(AItem)).Count();
-            //int totalBItems = items.Where(x => x.Equals(BItem)).Count();
-            //int totalCItems = items.Where(x => x.Equals(CItem)).Count();
-            //int totalDItems = items.Where(x => x.Equals(DItem)).Count();
-            //int totalEItems = items.Where(x => x.Equals(EItem)).Count();
-            //int totalFItems = items.Where(x => x.Equals(FItem)).Count();
+            int totalEItems = skusChar.Where(x => x.Equals('E')).Count();
 
             // calc total price of number of items
             foreach (char item in items.Keys)
             {
-            //    switch (item)
-            //    {
-            //        case 'A':
-            //            if (totalAItems % 5 == 0 || totalAItems > 5)
-            //            {
-            //                // offer: 5A for 200
-            //                int multiplier = totalAItems / 5;
-            //                totalPrice += (200 * multiplier);
-            //                totalAItems -= (5 * multiplier);
-            //            }
-            //            if (totalAItems % 3 == 0 || totalAItems > 3)
-            //            {
-            //                // offer: 3A for 130
-            //                int multiplier = totalAItems / 3;
-            //                totalPrice += (130 * multiplier);
-            //                totalAItems -= (3 * multiplier);
-            //            }
-            //            if (totalAItems > 0)
-            //                totalPrice += 50 * totalAItems;
-            //            continue;
-            //        case 'B':
-            //            // offer: buy one get B free
-            //            if (totalEItems >= 2)
-            //            {
-            //                int multiplier = totalEItems / 2;
-            //                totalPrice += 0;
-            //                for (int i=0; i<multiplier; i++)
-            //                {
-            //                    totalBItems--;
-            //                }
-            //            }
-            //            if (totalBItems % 2 == 0 || totalBItems > 2)
-            //            {
-            //                // offer : 2B for 45
-            //                int multiplier = totalBItems / 2;
-            //                totalPrice += (45 * multiplier);
-            //                totalBItems -= 2 * multiplier;
-            //            }
-            //            if (totalBItems > 0)
-            //                totalPrice += 30 * totalBItems;
-            //            continue;
-            //        case 'C':
-            //            totalPrice += 20 * totalCItems;
-            //            continue;
-            //        case 'D':
-            //            totalPrice += 15 * totalDItems;
-            //            continue;
-            //        case 'E':
-            //            totalPrice += 40 * totalEItems;
-            //            continue;
-            //        case 'F':
-            //            // offer: buy 2Fs and get another F free
-            //            if (totalFItems >= 3)
-            //            {
-            //                int multiplier = totalFItems / 3;
-            //                totalPrice += 0;
-            //                for (int i = 0; i < multiplier; i++)
-            //                {
-            //                    totalFItems--;
-            //                }
-            //            }
-            //            if (totalFItems > 0)
-            //                totalPrice += 10 * totalFItems;
-            //            continue;
-            //        default:
-            //            return -1;
-            //    }
+                switch (item)
+                {
+                    case 'A':
+                        int totalAItems = skusChar.Where(x => x.Equals(item)).Count();
+                        if (totalAItems % 5 == 0 || totalAItems > 5)
+                        {
+                            // offer: 5A for 200
+                            int multiplier = totalAItems / 5;
+                            totalPrice += (200 * multiplier);
+                            totalAItems -= (5 * multiplier);
+                        }
+                        if (totalAItems % 3 == 0 || totalAItems > 3)
+                        {
+                            // offer: 3A for 130
+                            int multiplier = totalAItems / 3;
+                            totalPrice += (130 * multiplier);
+                            totalAItems -= (3 * multiplier);
+                        }
+                        if (totalAItems > 0)
+                            totalPrice += 50 * totalAItems;
+                        continue;
+                    case 'B':
+                        int totalBItems = skusChar.Where(x => x.Equals(item)).Count();
+                        // offer: buy one get B free
+                        if (totalEItems >= 2)
+                        {
+                            int multiplier = totalEItems / 2;
+                            totalPrice += 0;
+                            for (int i = 0; i < multiplier; i++)
+                            {
+                                totalBItems--;
+                            }
+                        }
+                        if (totalBItems % 2 == 0 || totalBItems > 2)
+                        {
+                            // offer : 2B for 45
+                            int multiplier = totalBItems / 2;
+                            totalPrice += (45 * multiplier);
+                            totalBItems -= 2 * multiplier;
+                        }
+                        if (totalBItems > 0)
+                            totalPrice += 30 * totalBItems;
+                        continue;
+                    case 'C':
+                        int totalCItems = skusChar.Where(x => x.Equals(item)).Count();
+                        totalPrice += 20 * totalCItems;
+                        continue;
+                    case 'D':
+                        int totalDItems = skusChar.Where(x => x.Equals(item)).Count();
+                        totalPrice += 15 * totalDItems;
+                        continue;
+                    case 'E':
+                        totalPrice += 40 * totalEItems;
+                        continue;
+                    case 'F':
+                        int totalFItems = skusChar.Where(x => x.Equals(item)).Count();
+                        // offer: buy 2Fs and get another F free
+                        if (totalFItems >= 3)
+                        {
+                            int multiplier = totalFItems / 3;
+                            totalPrice += 0;
+                            for (int i = 0; i < multiplier; i++)
+                            {
+                                totalFItems--;
+                            }
+                        }
+                        if (totalFItems > 0)
+                            totalPrice += 10 * totalFItems;
+                        continue;
+                    default:
+                        return -1;
+                }
             }
             return totalPrice;
         }
